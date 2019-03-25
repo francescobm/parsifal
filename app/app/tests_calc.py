059d378d7f7353a17f972e04445c1fcc3a52bbb7
Francesco Greppi <francesco.greppi@backmarket.com>
2019-03-25 12:13:56 +0100

    chore(tests): Create a basic unit test

--- app/app/tests_calc.py
+++ app/app/tests_calc.py
@@ -2,8 +2,9 @@ from django.test import TestCase
 
 from app.calc import add
 
+
 class CalcTests(TestCase):
-  
-  def test_add_number(self):
-    """Test the sum works"""
-    self.assertEqual(add(3,4), 7)
+
+    def test_add_number(self):
+        """Test the sum works"""
+        self.assertEqual(add(3, 4), 7)
