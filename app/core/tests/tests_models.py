35d3f8aaad269addfc83dcc3a19492367f4fa38e
Francesco Greppi <francesco.greppi@backmarket.com>
2019-11-05 21:33:11 +0100

    feat(user): Add user and superuser creation and validation

--- app/core/tests/tests_models.py
+++ app/core/tests/tests_models.py
@@ -12,3 +12,24 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+    
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'myemail@UPPERCASE.COM'
+        user = get_user_model().objects.create_user(email=email, password='whatever')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_missing_email(self):
+        """Test creating user with no email raises an error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'whatever')
+    
+    def test_create_new_superuser(self):
+        """Test creating a new super user"""
+        user = get_user_model().objects.create_superuser('myemai@email.com', 'hello123')
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
+
